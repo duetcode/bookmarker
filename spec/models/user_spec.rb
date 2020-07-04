0d67bd23dfd07e0c0040988730a77f62e3817635
Faruk AYDIN <omerfaruk26@gmail.com>
2020-07-04 22:43:00 +0200

    Add validation tests for the User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+  end
 end
