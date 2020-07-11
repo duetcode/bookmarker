28debe4d5635ee0e4f52ebbf1ea8544b5d1b827e
Faruk AYDIN <omerfaruk26@gmail.com>
2020-07-11 11:02:58 +0200

    Refactor user creation test after introducing serializers

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe 'Api::V1::Users', type: :request do
       { email: 'user@duetcode.io', password: 'samplepassword' }
     end
 
-    it 'create a new user' do
+    it 'creates a new user' do
       post api_v1_users_path, params: { user: user_params }
       expected_body = { 'email' => 'user@duetcode.io' }
 
       expect(response).to have_http_status(:created)
-      expect(load_body(response)).to include(expected_body)
+      expect(load_body(response)['user']).to include(expected_body)
     end
 
     it 'returns unprocessable entity with errors' do
