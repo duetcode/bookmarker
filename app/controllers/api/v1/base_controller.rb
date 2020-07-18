bd2d9bbafab3f698e6bc5f138684ce94823824ef
Faruk AYDIN <omerfaruk26@gmail.com>
2020-07-18 16:24:03 +0300

    Include 'Renderer' module in base controller

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Api::V1::BaseController < ApplicationController
+  include Renderer
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Api::V1::UsersController < Api::V1::BaseController
-  include Renderer
-
   def create
     @user = User.new(user_params)
 
