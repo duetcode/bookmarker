88f44e866e001eb28046e806f88a38858b85f2ac
Faruk AYDIN <omerfaruk26@gmail.com>
2020-08-16 15:44:59 +0200

    Add localhost with 8080 port to allowed hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.hosts << 'http://localhost:8080'
 end
