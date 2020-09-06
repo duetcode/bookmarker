8ea7c976212e962edf3b8c2f6f49d5ecdad3ab96
Faruk AYDIN <omerfaruk26@gmail.com>
2020-09-06 16:46:34 +0200

    Add tests for the User model authenticate method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
   end
+
+  describe '#authenticate' do
+    let(:user) do
+      create(:user, email: 'user@duetcode.io', password: 'sample')
+    end
+
+    it 'returns user when the credentials are correct' do
+      expect(User.authenticate(user.email, user.password)).to eq(user)
+    end
+
+    it 'returns nil when the credentials are not correct' do
+      expect(User.authenticate(user.email, 'wrong')).to be_nil
+    end
+  end
 end
