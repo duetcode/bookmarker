bdf916b805097b7cd4bb834dc87f3b6dc54349bd
Faruk AYDIN <omerfaruk26@gmail.com>
2020-09-06 16:43:25 +0200

    Add authenticate class method to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  class << self
+    def authenticate(email, password)
+      user = User.find_for_authentication(email: email)
+      user.try(:valid_password?, password) ? user : nil
+    end
+  end
 end
